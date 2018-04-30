23e0f74997c1e88afe714f6357ecd8f01c5ba664
J. Pichardo <j.pichardo@tuta.io>
2018-04-30 11:10:14 -0700

    Services

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,12 @@
 export const config = {
   ip: '127.0.0.1',
-  port: 2222
+  port: 2222,
+  protoPath: '../proto',
+  protoServicesPaths: {
+    engineService: 'business/engine/executionService.proto',
+    petOwnershipService: 'service/petOwnership/petOwnershipService.proto',
+    petService: 'service/pet/petService.proto',
+    transactionService: 'service/transaction/transactionService.proto',
+    userService: 'service/user/userService.proto'
+  }
 };
--- src/registry.ts
+++ src/registry.ts
@@ -1,7 +1,61 @@
 import { require } from './utils/index';
 
+import * as grpc from 'grpc';
+
+function businessProtoPackageSelector(load, selector) {
+  return selector(load.jp.tamagotchi.business.shared.services);
+}
+
 export function registration(env) {
   return registry => {
     registry({ name: 'config', value: require(`config/config.${env}`) });
+    registry({
+      factory: resolve => {
+        const config = resolve('config');
+        const path = config.protoServicesPaths.petService;
+
+        return businessProtoPackageSelector(
+          grpc.load(`${config.protoPath}/${path}`),
+          protoPackage => protoPackage.pet
+        ).PetService.service;
+      },
+      name: 'sharedPetService'
+    });
+    registry({
+      factory: resolve => {
+        const config = resolve('config');
+        const path = config.protoServicesPaths.petService;
+
+        return businessProtoPackageSelector(
+          grpc.load(`${config.protoPath}/${path}`),
+          protoPackage => protoPackage.petOwnership
+        ).PetOwnershipService.service;
+      },
+      name: 'sharedPetOwnershipService'
+    });
+    registry({
+      factory: resolve => {
+        const config = resolve('config');
+        const path = config.protoServicesPaths.transactionService;
+
+        return businessProtoPackageSelector(
+          grpc.load(`${config.protoPath}/${path}`),
+          protoPackage => protoPackage.transaction
+        ).TransactionService.service;
+      },
+      name: 'sharedTransactionService'
+    });
+    registry({
+      factory: resolve => {
+        const config = resolve('config');
+        const path = config.protoServicesPaths.userService;
+
+        return businessProtoPackageSelector(
+          grpc.load(`${config.protoPath}/${path}`),
+          protoPackage => protoPackage.user
+        ).UserService.service;
+      },
+      name: 'sharedUserService'
+    });
   };
 }
