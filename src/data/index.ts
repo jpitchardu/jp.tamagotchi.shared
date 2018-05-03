36e9426570db3f9542871871c8e19f1c5e7fa91b
J. Pichardo <jonathan.pichardo@softtek.com>
2018-05-03 14:21:08 -0700

    Update Indexes

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,7 @@
 export * from './registration';
 
+export * from './dataContracts';
+
 export * from './petDataService';
 export * from './petOwnershipDataService';
 export * from './userDataService';
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,10 +2,13 @@ import { ServiceDefinition } from 'grpc';
 
 export * from './registration';
 
+export * from './serviceContracts';
+
 export * from './petService';
 export * from './petOwnershipService';
 export * from './transactionService';
 export * from './userService';
+
 export interface IServiceDefinition<T> {
   service: ServiceDefinition<T>;
   implementation: T;
